fadc7bf118a7179196625ca37e48fd8f370bacb6
hiraken <htb@mvf.biglobe.ne.jp>
2013-09-28 00:21:29 +0900

    record

--- Record/_07ViewController.h
+++ Record/_07ViewController.h
@@ -7,7 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <CoreAudio/CoreAudioTypes.h>
 
-@interface _07ViewController : UIViewController
+@interface _07ViewController : UIViewController  <AVAudioRecorderDelegate> {
+    
+    //AVAudioRecorder *recorder;
+    AVAudioRecorder *myRecorder;
+    NSURL *recordingURL;
+    NSURL *url;
+    bool *sw;
+
+    IBOutlet UIButton *b1;
+    IBOutlet UIButton *b2;
+    IBOutlet UIButton *b3;
+
+}
+
+- (IBAction)buttonClicked:(id)sender;
+- (IBAction)buttonClicked2:(id)sender;
+- (IBAction)buttonClicked3:(id)sender;
 
 @end
+
--- Record/_07ViewController.m
+++ Record/_07ViewController.m
@@ -8,22 +8,107 @@
 
 #import "_07ViewController.h"
 
-@interface _07ViewController ()
-
-@end
 
 @implementation _07ViewController
 
+- (IBAction)buttonClicked:(id)sender {
+    
+    NSArray *filePaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
+                                                             NSUserDomainMask,YES);
+    NSString *documentDir = [filePaths objectAtIndex:0];    
+    NSString *path = [documentDir stringByAppendingPathComponent:@"recording.caf"];
+    recordingURL = [NSURL fileURLWithPath:path];
+    
+    // 録音設定
+    NSDictionary *settings;
+    settings = [NSDictionary dictionaryWithObjectsAndKeys:
+                [NSNumber numberWithUnsignedInt:kAudioFormatLinearPCM], AVFormatIDKey,
+                [NSNumber numberWithInt:1], AVNumberOfChannelsKey,
+                nil];
+    
+    // インスタンス生成
+    NSError *error = nil;
+    myRecorder = [[AVAudioRecorder alloc]initWithURL:recordingURL
+                                            settings:settings 
+                                            error:&error];
+    
+     if(error){
+     NSLog(@"error = %@",error);
+     return;
+     }
+     
+    
+/*
+     b1 = nil;
+     b3 = nil;
+     
+     // 録音データを保存する場所
+     NSString *path = [NSString stringWithFormat:@"%@/test.caf",
+     [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"]];
+     
+     url = [[NSURL alloc] initFileURLWithPath:path];
+     // 録音の設定 AVNumberOfChannelsKey チャンネル数1
+     NSDictionary *settings = [NSDictionary dictionaryWithObjectsAndKeys:
+     [NSNumber numberWithFloat: 44100.0], AVSampleRateKey,
+     [NSNumber numberWithInt: kAudioFormatLinearPCM], AVFormatIDKey,
+     [NSNumber numberWithInt: 1], AVNumberOfChannelsKey,
+     [NSNumber numberWithInt:16], AVLinearPCMBitDepthKey,
+     [NSNumber numberWithBool:NO], AVLinearPCMIsBigEndianKey,
+     [NSNumber numberWithBool:NO], AVLinearPCMIsFloatKey,
+     nil];
+     // インスタンス生成(エラー処理は省略)
+     NSError *error = nil;
+     recorder = [[AVAudioRecorder alloc] initWithURL:url settings:settings error:&error];
+     recorder.delegate = self;
+     // 録音ファイルの準備(すでにファイルが存在していれば上書きしてくれる)
+     [recorder prepareToRecord];
+     // 録音中に音量をとるかどうか
+     recorder.meteringEnabled = YES;
+     
+     //sw = FALSE;
+ */   
+    //recorder.delegate = self;
+    myRecorder.delegate = self;     
+    //[recorder record];
+    [myRecorder record];
+        
+
+}
+
+- (IBAction)buttonClicked2:(id)sender {
+    /*
+    url = recorder.url;
+    AVAudioPlayer *player = [[AVAudioPlayer alloc]initWithContentsOfURL:url
+                             
+                                                                  error:nil];
+
+    [player play];
+     */
+    recordingURL = myRecorder.url;
+    //recordingURL = recorder.url;
+    AVAudioPlayer *player = [[AVAudioPlayer alloc]initWithContentsOfURL:recordingURL error:nil];
+    //player.delegate = self;
+    [player play];
+}
+
+- (IBAction)buttonClicked3:(id)sender{
+    // 録音終了
+    //[recorder stop];
+    [myRecorder stop];
+    // 録音データの削除。stop メソッドを呼ぶ前に呼んではいけない
+    //[myRecorder deleteRecording];
+    //[recorder deleteRecording];
+}
+
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+        
 }
 
 - (void)viewDidUnload
 {
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
+        // 録音ファイルパス
+       
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
